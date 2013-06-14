9010a8053026f37d4bd6666ece7e1db9003b6e31
Ishaan Bahal <ishaanbahal@gmail.com>
2013-06-14 16:03:46 +0530

    Version 2.0 GUI Almost Complete, Log problematic.

--- ChatGUI2.java
+++ ChatGUI2.java
@@ -14,54 +14,59 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+public class ChatGUI2 extends Menu {
 
-public class ChatGUI2  extends Menu {
-	
 	JFrame MainFrame = new JFrame("Chat Client");
 	static int val = 0;
 	JPanel panel1 = new JPanel();
+
 	JButton checkOnce = new JButton("Check Once");
-	JButton check = new JButton("Continuous Check");
-	JTextArea log = new JTextArea("Log");
-	public void go(){
-		log.setSize(350, 200);
+	JButton checkTimed = new JButton("Continuous Check");
+	JTextArea log = new JTextArea("Software Initialised.\n"
+			+ "Please choose an option", 10, 20);
+
+	public void go() {
+		panel1.setBackground(Color.WHITE);
 		panel1.add(checkOnce);
-		panel1.add(check);
-		//panel1.add(log);
-		MainFrame.setSize(400, 300);
+		panel1.add(checkTimed);
+		log.setEditable(false);
+		log.setLineWrap(true);
+		MainFrame.setSize(400, 250);
 		MainFrame.setResizable(false);
 		MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		MainFrame.setVisible(true);
-		MainFrame.add(BorderLayout.NORTH, bar);
-		MainFrame.add(BorderLayout.CENTER, panel1);
-		
-		//Method below is called from Menu Class to initialize the Menu B
+		MainFrame.getContentPane().add(BorderLayout.NORTH, bar);
+		MainFrame.getContentPane().add(BorderLayout.CENTER, panel1);
+		MainFrame.getContentPane().add(BorderLayout.SOUTH, log);
+
+		// Method below is called from Menu Class to initialize the Menu B
 		setFile();
-		
-		//Methods to fill the Menus
+
+		// Methods to fill the Menus
 		addItem("About", help);
 		addItem("Exit", file);
-		
-		//ActionListeners below.
-		
+
+		// ActionListeners below.
+
 		list.get(0).addActionListener(new helpClick());
 		list.get(1).addActionListener(new exitClick());
 		checkOnce.addActionListener(new CheckOnce());
-		
+		checkTimed.addActionListener(new CheckTimed());
 	}
-		
-	/*The asshole main() method..... Sometimes i wonder, 
-	 * why did they create a fucking main method...
-	 * just a damn run() or probably start() method
-	 * would be better. But well, here it is.*/
-	
-	
-	public static void main(String[] s){
+
+	/*
+	 * The asshole main() method..... Sometimes i wonder, why did they create a
+	 * fucking main method... just a damn run() or probably start() method would
+	 * be better. But well, here it is.
+	 */
+
+	public static void main(String[] s) {
 		ChatGUI2 gui = new ChatGUI2();
 		gui.go();
 	}
-	//Inner Class for ActionListener New in File Menu.
-	class helpClick implements ActionListener{
+
+	// Inner Class for ActionListener New in File Menu.
+	class helpClick implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -72,72 +77,81 @@ public class ChatGUI2  extends Menu {
 			aboutFrame.setResizable(false);
 			aboutFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			aboutFrame.setAlwaysOnTop(true);
-			JLabel label = new JLabel("<html><p><h4>This application has been developed by<br>" +
-					" Ishaan Bahal & Siddharth Jain<br>" +
-					"\n The Source Code for the application is open and can" +
-					" be found at <I><U><font color=\"Red\"><br>" +
-					"http://www.github.com/ishaanbahal</font></I></U><h3></p></html>");
+			JLabel label = new JLabel(
+					"<html><p><h4>This application has been developed by<br>"
+							+ " Ishaan Bahal & Siddharth Jain<br>"
+							+ "\n The Source Code for the application is open and can"
+							+ " be found at <I><U><font color=\"Red\"><br>"
+							+ "http://www.github.com/ishaanbahal</font></I></U><h3></p></html>");
 			aboutFrame.add(label);
-			
+
 		}
-		
+
 	}
-	//Inner class for ActionListener Exit in File Menu.
-	class exitClick implements ActionListener{
+
+	// Inner class for ActionListener Exit in File Menu.
+	class exitClick implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
+
 			MainFrame.dispose();
 		}
-		
+
 	}
-	class CheckOnce implements ActionListener{
+
+	class CheckOnce implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			INetChecker checker = new INetChecker();
-			if(checker.checkOnce()){
-				panel1.setBackground(new Color(0,40,154));
-			}
-			else {
-				panel1.setBackground(new Color(201,0,0));
+			if (checker.checkOnce()) {
+				if(log.getLineCount()>=8);
+				log.append("\nConnection Successful!\n"
+						+ "You can now enjoy your internet ");
+			} else {
+				log.append("\nSorry, Your internet is not working");
 			}
 		}
-		
+
 	}
-	class CheckTimed implements ActionListener{
+
+	class CheckTimed implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			INetChecker checker = new INetChecker();
-			if(checker.checkTimed(10)){
-				panel1.setBackground(new Color(0,40,154));
+			log.append("\nChecking you internet Connection\n"
+					+ "Connection Check Rate 10s\n"
+					+ "This method will keep running unless you close the program");
+			if (checker.checkTimed(10)) {
+				log.append("\n\nConnection Successful!\n"
+						+ "You can now enjoy your internet ");
 			}
-			
+
 		}
-		
+
 	}
 }
 
-abstract class Menu{
+abstract class Menu {
 	public JMenuBar bar = new JMenuBar();
 	public JMenu file = new JMenu("File");
 	public JMenu help = new JMenu("Help");
 	public ArrayList<JMenuItem> list = new ArrayList<JMenuItem>();
 	private static int num = 0;
-	
-	public void addItem(String s, JMenu a){
-		
-		list.add(num,new JMenuItem(s));
+
+	public void addItem(String s, JMenu a) {
+
+		list.add(num, new JMenuItem(s));
 		a.add(list.get(num));
 		num++;
-		
+
 	}
-	
-	public void setFile(){
+
+	public void setFile() {
 		bar.add(file);
 		bar.add(help);
 	}
-	
+
 }
